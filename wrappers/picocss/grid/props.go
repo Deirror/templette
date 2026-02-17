76da991ac5f40cf627e4b12e4eef59458173820c
Aleksandar Angelov <deirror.dev@gmail.com>
2026-02-17 19:56:03 +0200

    feat: add grid nav

--- wrappers/picocss/grid/props.go
+++ wrappers/picocss/grid/props.go
@@ -2,9 +2,14 @@ package grid
 
 import (
 	"github.com/Deirror/templette/components/card"
+	"github.com/Deirror/templette/components/semantic/nav"
 	"github.com/Deirror/templette/props"
 )
 
-var p card.Props = card.Props{
+var pCard card.Props = card.Props{
+	Attrs: props.Attrs{}.WithClass("grid"),
+}
+
+var pNav nav.Props = nav.Props{
 	Attrs: props.Attrs{}.WithClass("grid"),
 }
