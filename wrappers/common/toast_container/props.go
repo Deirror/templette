319bd3d9a03f26139b97bab016d6aed0fc240d62
Deirror <deirror.dev@gmail.com>
2026-03-13 17:42:36 +0200

    feat: add toast container

---
+++ wrappers/common/toast_container/props.go
@@ -0,0 +1,10 @@
+package toast_container
+
+import (
+	"github.com/Deirror/templette/components/card"
+	"github.com/Deirror/templette/props"
+)
+
+var p card.Props = card.Props{
+	Attrs: props.Attrs{}.WithID("toast-container"),
+}
