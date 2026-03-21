e5a6ccc64e030e0d398899ea52ccef78e4273dbe
Aleksandar Angelov <deirror.dev@gmail.com>
2026-03-21 10:43:57 +0200

    feat: add text left wrapper

---
+++ wrappers/common/text_left/props.go
@@ -0,0 +1,10 @@
+package text_left
+
+import (
+	"github.com/Deirror/templette/components/card"
+	"github.com/Deirror/templette/props"
+)
+
+var p card.Props = card.Props{
+	Attrs: props.Attrs{}.WithStyle("text-align: left"),
+}
