26bd15873a650a1d85f4d665a23b21a361c4264b
Aleksandar Angelov <deirror.dev@gmail.com>
2026-03-25 20:12:17 +0200

    feat: add fade in wrapper

---
+++ wrappers/common/fade_in/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2025 Deirror. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Package fade_in provides reusable Fade In wrapper components.
+// It defines a generic FadeIn wrapper around the Element function,
+// allowing different HTML tags to be used with shared props.
+// There are specific implementations for ease of use.
+package fade_in
---
+++ wrappers/common/fade_in/props.go
@@ -0,0 +1,8 @@
+// Copyright 2025 Deirror. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+package fade_in
+
+import "github.com/Deirror/templette/props"
+
+var Props props.Attrs = props.Attrs{}.WithClass("fade-in") // class style is defined in the host application
